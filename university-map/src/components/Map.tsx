48cd7fa27caab5fca59d5a125c257c7a38dfd449
Jack Lin <jacklin@cybersoft4u.com>
2024-02-15 00:11:08 +0800

    Add university data

--- university-map/src/components/Map.tsx
+++ university-map/src/components/Map.tsx
@@ -4,8 +4,8 @@ import 'leaflet/dist/leaflet.css';
 import { useTranslation } from 'react-i18next';
 
 const markerIcon = new L.Icon({
-  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconUrl: "/leaflet-color-markers/marker-icon-2x-blue.png",
+  shadowUrl: "/leaflet-color-markers/marker-shadow.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
